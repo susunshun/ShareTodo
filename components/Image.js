036ea15947f023689344ddedbaca0376c6c5a5d4
shun ono <susunshun@gmail.com>
2019-12-05 17:33:22 +0900

    Imageコンポーネントを追加

---
+++ components/Image.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.fileName = "./static/" + props.fileName;
+    this.size = props.size + "px";
+  }
+
+  render() {
+    return (
+      <img width={this.size} border="1" src={this.fileName} />
+    );
+  }
+}
+export default Image;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Link from "next/link";
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import Image from '../components/Image'
 import React from "react";
 import styled from 'styled-components';
 
 export default () => (
-    <Layout header="Next" title="Top Page">
-        <p>Welcome to next.js</p>
-        <TestText>styled component txt</TestText>
-        <div>
-          <Link href="/other">
-              go to other page
-          </Link>
-        </div>
-    </Layout>
+  <Layout header="Next" title="Top Page">
+    <p>Welcome to next.js</p>
+    <TestText>styled component txt</TestText>
+    <Image fileName="mokumoku.jpeg" size="250" />
+    <div>
+      <Link href="/other">
+        go to other page
+      </Link>
+    </div>
+  </Layout>
 );
 
 export const TestText = styled.div`
