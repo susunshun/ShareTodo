0ed4ce5fd5b38c29a9164748efe31b08c201747c
shun ono <susunshun@gmail.com>
2019-12-05 18:44:21 +0900

    NextにReduxをつなぐ

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Layout from '../components/Layout';
-import Image from '../components/Image'
+import Image from '../components/Image';
+import Counter from '../components/Counter';
 import React from "react";
 import styled from 'styled-components';
 
 export default () => (
   <Layout header="Next" title="Top Page">
+    <Counter />
     <p>Welcome to next.js</p>
     <TestText>styled component txt</TestText>
     <Image fileName="mokumoku.jpeg" size="250" />
--- pages/other.js
+++ pages/other.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Layout from '../components/Layout'
 import React from "react";
+import Counter from "../components/Counter";
 
 export default () => (
   <Layout header="Other" title="Other Page">
       <p>this is other page</p>
       <hr />
+      <Counter />
       <Link href="/">
           <button>back to main page</button>
       </Link>
